4615356a34251871ce6b68184e8d09d67f969df6
AKSHATA-HUB <111529628+AKSHATA-HUB@users.noreply.github.com>
2025-01-30 18:51:43 +0530

    files added

---
+++ TNS/src/com/java/Dayone/Dayone.java
@@ -0,0 +1,10 @@
+package com.java.Dayone;
+
+public class Dayone {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ TNS/src/dayone/dayone.java
@@ -0,0 +1,9 @@
+package dayone;
+
+public class dayone {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
