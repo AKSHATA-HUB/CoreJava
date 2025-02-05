73238e0d4ffa6bf40965c4dd70c214be6ea770e5
AKSHATA-HUB <111529628+AKSHATA-HUB@users.noreply.github.com>
2025-02-05 14:16:48 +0530

    Day two added

---
+++ TNS/src/DayTwo/DecisonMaking.java
@@ -0,0 +1,25 @@
+package DayTwo;
+
+public class DecisonMaking {
+
+	public static void main(String[] args) {
+		int x=10,y=4;
+		int a=10;
+		int b=12;
+		if(x>=y) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		if(!(a<b)||(a==b)) {
+			System.out.println("Condition is True");
+		}
+		else {
+			System.out.println("Condition is false");
+		}
+		
+
+	}
+
+}
--- TNS/src/com/java/TypeCasting/TypeCasting.java
+++ TNS/src/com/java/TypeCasting/TypeCasting.java
@@ -38,5 +38,6 @@ public class TypeCasting {
 		System.out.println(qwe);
 
 	}
+	Scanner
 
 }
